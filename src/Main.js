bcb7b5d020929de6276a15c1a6908353fe861898
yuki <s1932058jl@s.chibakoudai.jp>
2021-08-02 10:55:11 +0900

    切り替え方の変更

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,30 @@
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
-import App from "./App"
+import App from "./App";
 
-function Main () {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/content">Content</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/content">
-                        <Home />
-                    </Route>
-                    <Route path="/">
-                        <App />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    )
+function Main() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li className="py-2 px-4  bg-gradient-to-r from-green-400 to-blue-500 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+                <Link to="/content">Content</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/content" component={Home} />
+            <Route path="/" component={App} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default Main;
