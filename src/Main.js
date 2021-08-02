2efb3dcdb676db0bfd504c961f7109d522be8086
yuki <s1932058jl@s.chibakoudai.jp>
2021-08-02 11:30:28 +0900

    picture

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import App from "./App";
+import Sub from "./components/Sub";
 
 function Main() {
   return (
@@ -9,16 +10,20 @@ function Main() {
         <div>
           <nav>
             <ul>
-              <li>
+              <li className="py-2 px-4  bg-gradient-to-r from-violet-400 to-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
                 <Link to="/">Home</Link>
               </li>
               <li className="py-2 px-4  bg-gradient-to-r from-green-400 to-blue-500 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
                 <Link to="/content">Content</Link>
               </li>
+              <li className="py-2 px-4  bg-gradient-to-r from-yellow-600 to-yellow-100 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+                <Link to="/picture">Picture</Link>
+              </li>
             </ul>
           </nav>
           <Switch>
             <Route path="/content" component={Home} />
+            <Route path="/picture" component={Sub} />
             <Route path="/" component={App} />
           </Switch>
         </div>
---
+++ src/components/Sub.jsx
@@ -0,0 +1,11 @@
+import Picture from "../images/n1.jpg"
+
+function Sub () {
+    return (
+        <div>
+           <img src={Picture} alt="girl" />
+        </div>
+    )
+}
+
+export default Sub
